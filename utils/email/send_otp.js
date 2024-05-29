2b08bda3359e4693a1d5a49925abcb520fa215f1
exoream <shiigetsu1@gmail.com>
2024-05-29 14:24:06 +0800

    bismillah

--- utils/email/send_otp.js
+++ utils/email/send_otp.js
@@ -4,8 +4,8 @@ const path = require("path");
 require("dotenv").config();
 
 const transporter = createTransport({
-  host:  process.env.EMAIL_HOST,
-  port: process.env.EMAIL_PORT,
+  host: process.env.EMAIL_HOST,
+  port: 587,
   secure: false,
   auth: {
     user: process.env.EMAIL_USER,
